b721d1efe9e6ae8d5e8095be36080718ef200fb9
miUsu <josekber1@gmail.com>
2025-04-09 18:57:46 -0500

    se agrego texto pene

--- src/githubestructuras/GithubEstructuras.java
+++ src/githubestructuras/GithubEstructuras.java
@@ -5,6 +5,7 @@ public class GithubEstructuras {
 
     public static void main(String[] args) {
         System.out.println("este es un cambio");
+        System.out.println("pene");
     }
     
 }
